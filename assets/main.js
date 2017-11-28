313ae09d0fb4cbe0ce532f28f38e58b5101abe2c
Omar Tapia-Santacruz <omar.tsc@gmail.com>
2017-11-28 18:10:16 +0100

    an element per completed course

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,13 @@ $(function() {
   // your code will go here
     $.ajax('https://www.codeschool.com/users/tapior.json', {
         dataType: 'jsonp',
-        success: function(result) {}
+        success: function(response) {
+            var compCourses = $.map(response.courses.completed,function(index,course) {
+                var item = $('<div class=".course"> </div>');
+                item.append(course);
+                return item;
+            });
+            $('#badges').appendChild(compCourses);
+        }
     });
 });
