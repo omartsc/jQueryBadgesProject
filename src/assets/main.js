a6705b8cd22209344d44191e861527e0d3fb952a
Omar Tapia-Santacruz <omar.tsc@gmail.com>
2017-11-28 17:49:52 +0100

    make an ajax call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,8 @@
 $(function() {
 
   // your code will go here
-
+    $.ajax('https://www.codeschool.com/users/tapior.json', {
+        dataType: 'jsonp',
+        success: function(result) {}
+    });
 });
